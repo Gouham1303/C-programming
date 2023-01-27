d5ed2f7d3da248c5daae927817f5014972310553
Gouham1303 <123446989+Gouham1303@users.noreply.github.com>
2023-01-27 14:03:36 +0530

    Add files via upload

---
+++ sum using pointers.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a,b,*p,*q,sum;
+	printf("enter the value of a:");
+	scanf("%d",&a);
+	printf("enter the value of b:");
+	scanf("%d",&b);
+	p=&a;
+	q=&b;
+	sum=*p+*q;
+	printf("the sum is %d",sum);
+	
+}
